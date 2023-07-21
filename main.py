989c69ddb3f8052a988d39b127d4fa97cafb0f8f
alisadaintanvir <alisadaintanvir@gmail.com>
2023-07-21 22:53:52 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import smtplib
+import datetime as dt
+import random
+
+
+def send_mail(message):
+    my_email = "freebotpubg@gmail.com"
+    password = 'fginijsbvvdgqxma'
+    receiver_email = "alisadaintanvir@gmail.com"
+
+    try:
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(my_email, password)
+            connection.sendmail(my_email, receiver_email, message)
+            print("Email sent successfully!")
+    except smtplib.SMTPException as e:
+        print(f"An error occurred while sending the email: {e}")
+
+
+now = dt.datetime.now()
+week_day = now.weekday()
+
+if week_day == 4:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quotes = random.choice(all_quotes)
+        send_mail(f"Subject:Friday Quotes \n\n {quotes}")
